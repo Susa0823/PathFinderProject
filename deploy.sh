8dd52d143fd7c3e2678f996ba3e92053ac3195e3
Abdulla <axa1808@student.bham.ac.uk>
2023-03-14 16:33:40 +0400

    Updated ci/cd trying to deploy app onto vm

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 psql -V > /dev/null 2>&1
 
-ssh -o StrictHostKeyChecking=no root@143.198.222.14 << 'INITSSH'
-    cd /PathFinder
-    docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-    docker pull $IMAGE:web
-    docker-compose -f docker-compose.yml up -d
-INITSSH
+# ssh -o StrictHostKeyChecking=no root@143.198.222.14 << 'INITSSH'
+#     cd /PathFinder
+#     docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+#     docker pull $IMAGE:web
+#     docker-compose -f docker-compose.yml up -d
+# INITSSH
 # `$?` for the exit status of the last command
 echo "This deploy script is under development ¯\(°_o)/¯"
